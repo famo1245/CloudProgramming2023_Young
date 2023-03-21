df777051cf2ada64a8b287c23c573662062ef4e3
famo1245 <kandallee38@gmail.com>
2023-03-21 16:56:07 +0900

    블로그 목록 뷰를 만들었다네

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+from .models import Post
+
+
 # Create your views here.
+
+
+def index(request):
+    posts = Post.objects.all().order_by('-pk')  # 현재 DB에서 post class에 담겨 있는 모든 레코드를 꺼내달라
+    # order_by() -> 정렬, -pk -> pk에 대해서 역순 정렬
+    return render(request, 'blog/index.html', {  # 중괄호로 묶에서 보낼 수 있음
+        'posts': posts  # context로 넘겨줌
+    })
