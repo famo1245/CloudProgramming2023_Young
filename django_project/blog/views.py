e5d2f254ce2a351c444184e3bd11aaa9ae88e519
famo1245 <kandallee38@gmail.com>
2023-03-28 15:39:40 +0900

    CBV refactoring

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,25 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
 from .models import Post
 
 
-# Create your views here.
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'    # 리스트의 순서를 pk의 역순으로
 
 
-def index(request):
-    posts = Post.objects.all().order_by('-pk')  # 현재 DB에서 post class에 담겨 있는 모든 레코드를 꺼내달라
-    # order_by() -> 정렬, -pk -> pk에 대해서 역순 정렬
-    return render(request, 'blog/index.html', {  # 중괄호로 묶에서 보낼 수 있음
-        'posts': posts  # context로 넘겨줌
-    })
-
-
-def single_post_page(request, post_num):
-    post = Post.objects.get(pk=post_num)
-
-    return render(
-        request,
-        'blog/single_post_page.html', {
-            'post': post
-        }
-    )
+class PostDetail(DetailView):
+    model = Post
